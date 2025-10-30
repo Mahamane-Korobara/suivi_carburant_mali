c5e71c026dc02fe6dac52299f39a35368363391c
Mahamane-Korobara <korobaramahamane.311@gmail.com>
2025-10-30 14:02:08 +0100

    Initialisation du projet Laravel Suivi Carburant

---
+++ bootstrap/app.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function ($middleware) {
+        $middleware->statefulApi([
+            EnsureFrontendRequestsAreStateful::class,
+            EncryptCookies::class,
+            AddQueuedCookiesToResponse::class,
+            StartSession::class,
+        ]);
+    })
+    ->create();
